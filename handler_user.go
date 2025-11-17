c07e1686f23d8e84be25e7de1aeb26c99726f9c9
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-11-18 00:03:55 +0530

    added a way to create feeds and also created endpoint to get all feeds

--- handler_user.go
+++ handler_user.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/aditya-sutar-45/rss-aggregator/internal/auth"
 	"github.com/aditya-sutar-45/rss-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -39,18 +38,6 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 }
 
 // authenticated endpoint
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithErr(w, 403, fmt.Sprintf("Auth Error: %v\n", err))
-		return
-	}
-
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithErr(w, 404, fmt.Sprintf("User not found: %v\n", err))
-		return
-	}
-
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
---
+++ middleware_auth.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aditya-sutar-45/rss-aggregator/internal/auth"
+	"github.com/aditya-sutar-45/rss-aggregator/internal/database"
+)
+
+type authedHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetAPIKey(r.Header)
+		if err != nil {
+			respondWithErr(w, 403, fmt.Sprintf("Auth Error: %v\n", err))
+			return
+		}
+
+		user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithErr(w, 404, fmt.Sprintf("User not found: %v\n", err))
+			return
+		}
+
+		handler(w, r, user)
+	}
+}
